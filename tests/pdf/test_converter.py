38521387ce00712bbc241948cf464d4559e3fb2e
jordi <jordi.marsal@gmail.com>
2025-04-06 20:29:00 +0200

    chore: test converter

--- tests/pdf/test_converter.py
+++ tests/pdf/test_converter.py
@@ -1,6 +1,7 @@
 import os
 import unittest
-from unittest.mock import MagicMock
+from typing import Any
+from unittest.mock import MagicMock, mock_open, patch
 
 from src.core.models import PdfCfg
 from src.markdown.processor import MarkdownProcessor
@@ -9,6 +10,9 @@ from src.pdf.converter import PdfConverter
 
 class TestPdfConverter(unittest.TestCase):
     def setUp(self) -> None:
+        os.makedirs("tests/resources", exist_ok=True)
+        with open("tests/resources/test.svg", "w") as f:
+            f.write("<svg></svg>")  # Contingut mínim d'un fitxer SVG
         # Configuració inicial per als tests
         self.cfg = PdfCfg(
             md_path="tests/output/test.md",
@@ -23,12 +27,14 @@ class TestPdfConverter(unittest.TestCase):
         self.mock_svg_files = ["tests/resources/test.svg"]
         self.mock_temp_md_path = "temp.md"
 
-    def test_convert_to_pdf(self) -> None:
+    @patch("os.remove")
+    @patch("builtins.open", new_callable=mock_open, read_data="Mocked file content")
+    @patch("src.pdf.converter.md2pdf")
+    def test_convert_to_pdf(self,mock_md2pdf:Any, mock_open: Any, mock_remove:Any) -> None:
         # Mock the methods to avoid actual file operations
         self.processor.process_markdown = MagicMock(return_value=(self.markdown_content, self.mock_svg_files))
         os.makedirs = MagicMock()
-        open_ = MagicMock()
-        md2pdf = MagicMock()
+
 
         # Call the method to test
         self.converter.convert_to_pdf(self.markdown_content)
@@ -36,8 +42,17 @@ class TestPdfConverter(unittest.TestCase):
         # Check if the methods were called correctly
         self.processor.process_markdown.assert_called_once_with(self.markdown_content)
         os.makedirs.assert_called_once_with(os.path.dirname(self.cfg.tmp_md_path), exist_ok=True)
-        open_.assert_called_once_with(self.cfg.tmp_md_path, "w")
-        md2pdf.assert_called_once_with(
+
+
+        # Verify all calls to os.remove
+        mock_remove.assert_has_calls([
+            unittest.mock.call(self.mock_svg_files[0]),  # El fitxer SVG
+            unittest.mock.call(self.cfg.tmp_md_path),   # El fitxer temporal
+        ], any_order=True)
+
+        # Verify the call to open
+        mock_open.assert_called_once_with(self.cfg.tmp_md_path, "w")
+        mock_md2pdf.assert_called_once_with(
             self.cfg.pdf_path,
             md_file_path=self.cfg.tmp_md_path,
             css_file_path=self.cfg.css_path,
