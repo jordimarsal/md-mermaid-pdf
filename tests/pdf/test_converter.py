a4acc2fba27c876df5502f89746c37559a5c624b
jordi <jordi.marsal@gmail.com>
2026-02-16 11:25:10 +0100

    ci: fix end of files

--- tests/pdf/test_converter.py
+++ tests/pdf/test_converter.py
@@ -75,3 +75,4 @@ class TestPdfConverter(unittest.TestCase):
         with self.assertRaises(Exception) as cm:
             conv.convert_to_pdf(self.markdown_content)
         self.assertIn("Error writing temp file", str(cm.exception))
+
